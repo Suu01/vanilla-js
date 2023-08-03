f12d3d586755df215a10d696ee218609930610ca
Suu01 <92988188+Suu01@users.noreply.github.com>
2023-08-03 16:24:23 +0900

    clock and greeting

--- 5/js/clock.js
+++ 5/js/clock.js
@@ -1,13 +1,13 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-    const date = new Date();
+  const date = new Date();
 
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 getClock(); // setInterval만 사용하면 1초 후에 시계가 뜨기 시작하기 때문에 웹사이트가 load 되자마자 함수를 실행하고 그 후에 setInterval()로 매초마다 다시 실행하게 함
 setInterval(getClock, 1000);
